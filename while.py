1471b388ef988536e29f0eedcd3afa60fa591883
Jenya <jenechka0109@gmail.com>
2023-05-03 00:01:03 -0700

    While

--- while.py
+++ while.py
@@ -1,14 +1,9 @@
 n=int(input('Введіть число:'))
-i = 1
-for i in range(n+1):
-   while i % 3 == 0:
-       print(i)
-       i += 1
-
-n=int(input('Введіть число:'))
-s = 0
-for i in range(n+1):
-    while i % 3 == 0:
-        s += i
-        i += 1
-print(s)
+i=0
+sum=0
+while i <= n:
+    if i % 3 == 0:
+        sum+= i
+        print(i)
+    i += 1
+print('Summa:', sum)
