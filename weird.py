6952dbd76ed4ac4b2dd43f06382e4cca25f57e3f
Jenya <jenechka0109@gmail.com>
2023-06-10 19:47:53 -0700

    weird-not.weird

---
+++ weird.py
@@ -0,0 +1,10 @@
+number = int(input('Enter your number: '))
+n = number % 2
+if n == 1 and number > 20:
+    print('Not Weird')
+elif n == 0 and 2 <= number <= 5:
+    print('Not Weird')
+elif n == 0 and 6 <= number <= 20:
+    print('Weird')
+elif n == 1:
+    print('Weird')
